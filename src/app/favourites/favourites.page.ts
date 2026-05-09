4de33e2f4d8cfdf9896194cc830b2af8b4b93bb0
Charles O <charlesolasubomi@gmail.com>
2026-05-09 16:36:40 +0200

    draft favourites, updated home, details

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -2,19 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterModule]
 })
-export class FavouritesPage implements OnInit {
+export class FavouritesPage {
+  favouriteMovies: any[] = [];
 
   constructor() { }
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.loadFavourites();
   }
 
+  loadFavourites() {
+    this.favouriteMovies = JSON.parse(localStorage.getItem('favourites') || '[]');
+  }
 }
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MovieService } from '../service/movie'; 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  movies: any[] = [];
+  searchTerm: string = '';
+  studentNumber: string = 'G00473370'; 
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit() {
+    this.loadTrending();
+  }
+
+  loadTrending() {
+    this.movieService.getTrendingMovies().subscribe((res: any) => {
+      this.movies = res.results;
+    });
+  }
+
+  onSearchChange(event: any) {
+    this.searchTerm = event.detail.value;
+    if (this.searchTerm.trim() === '') {
+      this.loadTrending();
+    } else {
+      this.movieService.searchMovies(this.searchTerm).subscribe((res: any) => {
+        this.movies = res.results;
+      });
+    }
+  }
 }
