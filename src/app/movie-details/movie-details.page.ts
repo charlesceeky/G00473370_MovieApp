916037a85cc7affeba0028183aa802971c101983
Charles O <charlesolasubomi@gmail.com>
2026-05-10 19:48:19 +0100

    Misc updates, pulled genre and release date

--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 import { MovieService } from '../service/movie';
 import { addIcons } from 'ionicons';
-import { heart, heartOutline, home } from 'ionicons/icons';
+import { heart, heartOutline, star, list, listOutline, home } from 'ionicons/icons';
 
 @Component({
   selector: 'app-movie-details',
@@ -17,16 +17,19 @@ export class MovieDetailsPage implements OnInit {
   movie: any = null;
   cast: any[] = [];
   crew: any[] = [];
+  
   isFavourite: boolean = false;
+  isInList: boolean = false; // NEW: Track list status
 
-  castLimit: number = 5; 
+  castLimit: number = 5;
   crewLimit: number = 5;
 
   constructor(
     private route: ActivatedRoute,
     private movieService: MovieService
   ) {
-    addIcons({ heart, heartOutline, home });
+
+    addIcons({ heart, heartOutline, star, list, listOutline, home }); 
   }
 
   ngOnInit() {
@@ -35,6 +38,7 @@ export class MovieDetailsPage implements OnInit {
       this.movieService.getMovieDetails(id).subscribe(res => {
         this.movie = res;
         this.checkFavourite();
+        this.checkList(); // NEW: Check if it's in the list on load
       });
       this.movieService.getMovieCredits(id).subscribe((res: any) => {
         this.cast = res.cast;
@@ -43,6 +47,7 @@ export class MovieDetailsPage implements OnInit {
     }
   }
 
+  // --- FAVOURITES LOGIC ---
   checkFavourite() {
     const favs = JSON.parse(localStorage.getItem('favourites') || '[]');
     this.isFavourite = favs.some((f: any) => f.id === this.movie.id);
@@ -59,8 +64,25 @@ export class MovieDetailsPage implements OnInit {
     this.isFavourite = !this.isFavourite;
   }
 
+  // --- NEW: WATCHLIST LOGIC ---
+  checkList() {
+    const watchList = JSON.parse(localStorage.getItem('watchlist') || '[]');
+    this.isInList = watchList.some((m: any) => m.id === this.movie.id);
+  }
+
+  toggleList() {
+    let watchList = JSON.parse(localStorage.getItem('watchlist') || '[]');
+    if (this.isInList) {
+      watchList = watchList.filter((m: any) => m.id !== this.movie.id);
+    } else {
+      watchList.push(this.movie);
+    }
+    localStorage.setItem('watchlist', JSON.stringify(watchList));
+    this.isInList = !this.isInList;
+  }
+
+  // --- ACCORDION LOGIC ---
   toggleCastLimit() {
-    // If it's currently 5, show all. If it's showing all, revert to 5.
     this.castLimit = this.castLimit === 5 ? this.cast.length : 5;
   }
 
